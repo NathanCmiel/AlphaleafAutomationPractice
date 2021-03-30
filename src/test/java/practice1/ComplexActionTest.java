a61174f950fc6b9a7f7c95d17dd4b8dcb32a3404
Nathan Cmiel <ncmiel>
2021-03-29 21:09:16 -0400

    added another test case

--- src/test/java/practice1/ComplexActionTest.java
+++ src/test/java/practice1/ComplexActionTest.java
@@ -12,4 +12,13 @@ public class ComplexActionTest {
 
         System.out.println("Alphaleaf.");
     }
+
+    @Test
+    public void testCaseTwo(){
+        // Comment hello alphaleaf
+        // You guys rock! git branch is actually very hard concept
+        // to understand, just google it.
+        System.out.println("Git Branch is actually easy!");
+        System.out.println("Branch is separate version of project files and folders");
+    }
 }
